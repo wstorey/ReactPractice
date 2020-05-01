3de2b9f7ea7e5ed213c16b09e43c741aebbdd4fd
Will Storey <will@willstorey.ca>
2020-04-30 22:59:10 -0400

    Finished jsx doc

--- DocsPrac/HelloWorld/main.js
+++ DocsPrac/HelloWorld/main.js
@@ -1,7 +1,82 @@
 'use strict';
 
+//! Basic rendering with jsx
+
 ReactDOM.render(React.createElement(
     'h1',
     null,
     'Hello, World'
 ), document.querySelector('#root'));
+
+//! Rendering with vars
+var name = 'Will Storey';
+var element = React.createElement(
+    'p',
+    null,
+    'Hello, ',
+    name
+);
+
+ReactDOM.render(element, document.querySelector('#jsx'));
+
+//! Rendering with a function
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+var user = {
+    firstName: 'John',
+    lastName: 'Oliver'
+};
+
+var elementTwo = React.createElement(
+    'p',
+    null,
+    'Hello, ',
+    formatName(user)
+);
+
+ReactDOM.render(elementTwo, document.querySelector('#jsxTwo'));
+
+//! jsx are attributes! We can use ifs and fors
+function getGreeting(user) {
+    if (user) {
+        return React.createElement(
+            'p',
+            null,
+            'Hello, ',
+            formatName(user),
+            '!!'
+        );
+    } else {
+        return React.createElement(
+            'p',
+            null,
+            'Hello, STRANGER'
+        );
+    }
+}
+
+ReactDOM.render(getGreeting(user), document.querySelector('#jsx3'));
+
+//! Pass data attribute with "" and can close a tag like normal
+var dataAttr = React.createElement('div', { tabIndex: '0' });
+var imgEx = React.createElement('img', { src: user.profileImageUrl });
+
+//! jsx can have children el's with a class and id
+var children = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        { 'class': 'greeting' },
+        'Hello!'
+    ),
+    React.createElement(
+        'h2',
+        { id: 'subtitle' },
+        'Good to see you here.'
+    )
+);
+
+ReactDOM.render(children, document.querySelector('#jsx4'));
--- src/DocsPrac/HelloWorld/main.js
+++ src/DocsPrac/HelloWorld/main.js
@@ -1,6 +1,69 @@
 'use strict';
 
+//! Basic rendering with jsx
 ReactDOM.render(
     <h1>Hello, World</h1>,
     document.querySelector('#root')
 );
+
+//! Rendering with vars
+const name = 'Will Storey';
+const element = <p>Hello, { name }</p>;
+
+ReactDOM.render(
+    element,
+    document.querySelector('#jsx')
+)
+
+//! Rendering with a function
+function formatName(user) {
+    return `${user.firstName} ${user.lastName}`;
+}
+
+const user = {
+    firstName: 'John',
+    lastName: 'Oliver'
+};
+
+const elementTwo = (
+    <p>
+        Hello, { formatName(user) }
+    </p>
+);
+
+ReactDOM.render(
+    elementTwo,
+    document.querySelector('#jsxTwo')
+);
+
+//! jsx are attributes! We can use ifs and fors
+function getGreeting(user) {
+    if (user) {
+    return <p>Hello, { formatName(user) }!!</p>
+    } else {
+        return <p>Hello, STRANGER</p>
+    }
+}
+
+ReactDOM.render(
+    getGreeting(user),
+    document.querySelector('#jsx3')
+);
+
+//! Pass data attribute with "" and can close a tag like normal
+const dataAttr = <div tabIndex="0"></div>;
+const imgEx = <img src={ user.profileImageUrl }/>;
+
+//! jsx can have children el's with a class and id
+const children = (
+    <div>
+        <h1 class="greeting">Hello!</h1>
+        <h2 id="subtitle">Good to see you here.</h2>
+    </div>
+);
+
+ReactDOM.render(
+    children,
+    document.querySelector('#jsx4')
+)
+
